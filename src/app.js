f89896b79f99d86961ce246e9c75afa210ccafa3
pepeyen <erickfcaldeira@gmail.com>
2020-10-27 16:27:47 -0300

    Initial commit

---
+++ src/app.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const morgan = require('morgan');
+
+const app = express();
+const port = 8080;
+
+app.use(morgan('dev'));
+app.use(express.static(__dirname + '../public'));
