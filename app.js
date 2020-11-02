651997749ba70de597f181e451307312915840f4
pepeyen <erickfcaldeira@gmail.com>
2020-11-01 21:43:02 -0300

    Introduced database based API and HttpOnly cookies to the Auth flow

--- app.js
+++ app.js
@@ -1,14 +1,18 @@
 require("dotenv-safe").config();
-
 const express = require('express');
-const morgan = require('morgan');
-const bodyparser = require('body-parser');
-const jwt = require('jsonwebtoken'); 
-const cors = require('cors');
 
 const app = express();
+
+//Add-ons
+const morgan = require('morgan');
+const cors = require('cors');
+const bodyparser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const jwt = require('jsonwebtoken');
+const mysql = require('mysql');
+
 const verifyJWT = (req, res, next) => {
-    const access_token = req.headers['access-token'];
+    const access_token = req.cookies.access_token;
 
     if(!access_token){
         return res.status(401).json({
@@ -30,65 +34,88 @@ const verifyJWT = (req, res, next) => {
 };
 
 app.use(morgan('dev'));
-app.use(cors());
+app.use(cors({
+    origin: `${process.env.ORIGIN_ADDRESS || 'http://localhost'}:${process.env.ORIGIN_PORT || 3000}`,
+    credentials: true
+}));
+app.use(cookieParser());
 app.use(bodyparser.urlencoded({extended : false}));
 app.use(bodyparser.json());
 
+const connection = mysql.createConnection({
+    host: process.env.DATABASE_HOST,
+    user: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE_NAME
+});
+
+connection.connect((error) => {
+    if(error){
+        throw error;
+    }
+});
+
+//Server routes
 app.get('/',function(req,res){
     res.status(200).send(`Welcome to login`);
 });
 
 app.post('/api/login',(req,res) => {
-    if(req.body.email === 'efrederick.dev@gmail.com' && req.body.password === 'pepeyen'){
-        /**
-         * This will be handled by the database
-         * For now i will use mock up data
-         */
-        const user_id = 42069;
+    connection.query(`SELECT user_id FROM user_table WHERE user_email = '${req.body.email}' AND user_password = '${req.body.password}'`, (error,result) => {
+        if(error){
+            res.status(500).json({
+                success: false,
+                description: 'Server error, please try again'
+            });
+        }
 
-        const access_token = jwt.sign({user_id}, process.env.SECRET, {
-            expiresIn: 600 //10 minutes
-        });
-        res.status(200).json({
-            success: true,
-            access_token: access_token
-        });
-    }else{
-        res.status(401).json({
-            success: false,
-            description: 'Invalid username or password'
-        });
-    }
+        if(result.length === 0){
+            res.status(401).json({
+                success: false,
+                description: 'Invalid username or password'
+            });
+        }else{
+            const userId = result[0].user_id;
+
+            const access_token = jwt.sign({userId}, process.env.SECRET, {
+                expiresIn: 600
+            });
+
+            res.cookie('access_token', access_token, {
+                httpOnly: true, 
+                secure: true
+            });
+            res.status(200).json({
+                success: true,
+                loggedUserId: userId
+            });
+        }
+    });
 });
 
-app.get('/api/client/:client_id', verifyJWT, (req, res, next) => {
-    /**
-     * This will be handled by the database
-     * For now i will use mock up data
-     */
-    switch (req.params.client_id) {
-        case '24':
-            return res.status(200).json({
-                success: true,
-                user_info: {
-                    user_id: 24,
-                    user_name:'Lusquete'
-                }
+app.get('/api/client/:clientId', verifyJWT, (req, res, next) => {
+    connection.query(`SELECT * FROM user_table WHERE user_id = ${req.params.clientId}`, (error,result) => {
+        if(error){
+            res.status(500).json({
+                success: false,
+                description: 'Server error, please try again'
             });
-        case '42069':
-            return res.status(200).json({
-                success: true,
-                user_info: {
-                    user_id: 42069,
-                    user_name: 'Pepeyen'
-                }
-            });
-        default:
-            return res.status(404).json({
+        }
+        if(result.length === 0){
+            res.status(401).json({
                 success: false,
                 description: 'User not found'
             });
-    }
+        }else{
+            res.status(200).json({
+                success: true,
+                user_info: {
+                    userId: result[0].user_id,
+                    userName: result[0].user_name
+                }
+            });
+        }
+    });
 });
 
 const port = process.env.PORT||8080;
