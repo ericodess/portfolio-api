6b35dbb47796c6e77c9bdea5c628b791ea41ec7b
pepeyen <erickfcaldeira@gmail.com>
2020-11-05 16:10:31 -0300

    Major routing file structure update

--- app.js
+++ app.js
@@ -8,31 +8,12 @@ const morgan = require('morgan');
 const cors = require('cors');
 const bodyparser = require('body-parser');
 const cookieParser = require('cookie-parser');
-const jwt = require('jsonwebtoken');
-const mysql = require('mysql');
 
-const verifyJWT = (req, res, next) => {
-    const access_token = req.cookies.access_token;
 
-    if(!access_token){
-        return res.status(401).json({
-            success: false,
-            description: 'No access_token provided'
-        });
-    }
-
-    jwt.verify(access_token, process.env.SECRET, (err,decoded) => {
-        if(err){
-            return res.status(500).json({
-                success: false,
-                description: 'Failed to authenticate access_token'
-            });
-        }
-        req.user_id = decoded.user_id;
-        next();
-    });
-};
-
+//Routes
+const root = require('./routes/root');
+const auth = require('./routes/auth');
+const users = require('./routes/users');
 app.use(morgan('dev'));
 app.use(cors({
     origin: `${process.env.ORIGIN_ADDRESS || 'http://localhost:3000'}`,
@@ -42,113 +23,9 @@ app.use(cookieParser());
 app.use(bodyparser.urlencoded({extended : false}));
 app.use(bodyparser.json());
 
-const pool = mysql.createPool({
-    connectionLimit: 10,
-    host: process.env.DATABASE_HOST,
-    user: process.env.DATABASE_USER,
-    password: process.env.DATABASE_PASSWORD,
-    database: process.env.DATABASE_NAME
-});
-
-const getConnection = () => {
-    return pool;
-}
-
-//Server routes
-app.get('/',function(req,res){
-    res.status(200).send(`Welcome to login`);
-});
-
-app.post('/api/auth',(req,res) => {
-    const connection = getConnection();
-
-    connection.query("SELECT user_name FROM user_table WHERE user_email LIKE BINARY ? AND user_password LIKE BINARY ?", [
-        req.body.email,
-        req.body.password
-    ],(error,result) => {
-        if(error){
-            res.status(500).json({
-                success: false,
-                description: 'Server error, please try again'
-            });
-        }
-
-        if(result.length === 0){
-            res.status(401).json({
-                success: false,
-                description: 'Invalid username or password'
-            });
-        }else{
-            const userName = result[0].user_name;
-
-            const access_token = jwt.sign({userName}, process.env.SECRET, {
-                expiresIn: 600
-            });
-
-            res.cookie('access_token', access_token, {
-                httpOnly: true, 
-                secure: true
-            });
-            res.status(200).json({
-                success: true,
-                loggedUser: userName
-            });
-        }
-    });
-});
-
-app.get('/api/users', (req, res, next) => {
-    const connection = getConnection();
-
-    connection.query("SELECT user_id,user_name FROM user_table", (error,result) => {
-        if(error){
-            res.status(500).json({
-                success: false,
-                description: 'Server error, please try again'
-            });
-        }
-        if(result.length === 0){
-            res.status(401).json({
-                success: false,
-                description: 'No users found'
-            });
-        }else{
-            res.status(200).json({
-                success: true,
-                user_info: result
-            });
-        }
-    });
-});
-
-app.get('/api/user/:clientName', (req, res, next) => {
-    const connection = getConnection();
-
-    connection.query("SELECT * FROM user_table WHERE user_name = ?", [
-        req.params.clientName
-    ], (error,result) => {
-        if(error){
-            res.status(500).json({
-                success: false,
-                description: 'Server error, please try again'
-            });
-        }
-        if(result.length === 0){
-            res.status(401).json({
-                success: false,
-                description: 'User not found'
-            });
-        }else{
-            res.status(200).json({
-                success: true,
-                user_info: {
-                    userId: result[0].user_id,
-                    userName: result[0].user_name
-                }
-            });
-        }
-    });
-});
+app.use('/', root);
+app.use('/api/auth', auth);
+app.use('/api/users', users);
 
 const port = process.env.PORT||8080;
 app.listen(port);
