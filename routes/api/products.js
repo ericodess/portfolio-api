9ae339fe370ffff6a247cefdf524a2a8db20aee3
pepeyen <erickfcaldeira@gmail.com>
2020-11-18 16:01:25 -0300

    Added new api product based end point

--- routes/api/products.js
+++ routes/api/products.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const querystring = require('querystring');
 
 //Models
 const getConnection = require('../../models/createPool');
@@ -6,56 +7,89 @@ const getQuery = require('../../models/createQuery');
 
 const router = express.Router();
 
-router.get('/', async (req, res) => {
-    const connection = getConnection();
-
-    await getQuery(connection, "SELECT * FROM products")
-    .then(result => {
-        if(result.length === 0){
-            res.status(404).json({
+router.get('/', (req, res) => {
+    getConnection(async (error,connection) => {
+        await getQuery(connection, "SELECT * FROM products")
+        .then(result => {
+            if(result.length === 0){
+                res.status(404).json({
+                    success: false,
+                    description: 'No products found'
+                });
+            }else{
+                res.status(200).json({
+                    success: true,
+                    products: result
+                });
+            }
+        })
+        .catch(error => {
+            res.status(500).json({
                 success: false,
-                description: 'No products found'
+                description: 'Server error, please try again'
             });
-        }else{
-            res.status(200).json({
-                success: true,
-                products: result
-            });
-        }
-    })
-    .catch(error => {
-        res.status(500).json({
-            success: false,
-            description: 'Server error, please try again'
-        });
-    })
+        })
+
+        connection.release();
+    });
 });
 
-router.get('/:productId', async (req, res) => {
-    const connection = getConnection();
-
-    await getQuery(connection, "SELECT * FROM products WHERE product_id = ?", [
-        req.params.productId
-    ])
-    .then(result => {
-        if(result.length === 0){
-            res.status(404).json({
+router.get('/offset/:productOffset', (req, res) => {
+    getConnection(async (error,connection) => {
+        await getQuery(connection, "SELECT * FROM products LIMIT ?", [
+            parseInt(req.params.productOffset)
+        ])
+        .then(result => {
+            if(result.length === 0){
+                res.status(404).json({
+                    success: false,
+                    description: 'No products found'
+                });
+            }else{
+                res.status(200).json({
+                    success: true,
+                    products: result
+                });
+            }
+        })
+        .catch(error => {
+            res.status(500).json({
                 success: false,
-                description: 'Product not found'
+                description: 'Server error, please try again'
             });
-        }else{
-            res.status(200).json({
-                success: true,
-                products: result[0]
+        })
+
+        connection.release();
+    });
+});
+
+router.get('/:productId', (req, res) => {
+    getConnection(async (error,connection) => {
+        await getQuery(connection, "SELECT * FROM products WHERE product_id = ?", [
+            req.params.productId
+        ])
+        .then(result => {
+            if(result.length === 0){
+                res.status(404).json({
+                    success: false,
+                    description: 'Product not found'
+                });
+            }else{
+                res.status(200).json({
+                    success: true,
+                    products: result[0]
+                });
+            }
+        })
+        .catch(() => {
+            res.status(500).json({
+                success: false,
+                description: 'Server error, please try again'
             });
-        }
-    })
-    .catch(() => {
-        res.status(500).json({
-            success: false,
-            description: 'Server error, please try again'
-        });
-    })
+        })
+
+        connection.release();
+    });
 });
 
 module.exports = router;
