6797b5712f7f53b6533fdd0dd24c8f6af7588892
pepeyen <erickfcaldeira@gmail.com>
2021-02-16 16:52:35 -0300

    Logging out now removes the loggedUser cookie

--- services/admin/routes/admin/logout/logout.js
+++ services/admin/routes/admin/logout/logout.js
@@ -4,10 +4,12 @@ const router = express.Router();
 
 router.get('/', (req, res) => {
     const accessToken = req.cookies.access_token,
+          loggedUser = req.cookies.logged_user,
           loginPageURL = '/admin/login';
     
-    if(accessToken){
+    if(accessToken && loggedUser){
         res.clearCookie("access_token");
+        res.clearCookie("logged_user");
     };
 
     res.redirect(loginPageURL);
