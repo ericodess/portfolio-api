0c0c331ed35f090720d06b917b3895053498e1b1
pepeyen <erickfcaldeira@gmail.com>
2021-03-01 03:27:23 -0300

    universefy service file strutcture update

--- services/universefy/services/index.js
+++ services/universefy/services/index.js
@@ -1,12 +1,1 @@
-const generateRandomString = (length) => {
-	let text = '';
-	const possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-
-	for(let i = 0; i < length; i++) {
-		text += possible.charAt(Math.floor(Math.random() * possible.length));
-    }
-    
-	return text;
-};
-
-exports.generateRandomString = generateRandomString;
+exports.generateRandomString = require('./generateRandomString');
