b199428911478b4d98344be9e391be8881b98c42
Pepeyen <erickfcaldeira@gmail.com>
2021-07-01 16:48:06 -0300

    Introduced services Server - Client side file separation

---
+++ services/admin/pages/api/utils/index.ts
@@ -0,0 +1,2 @@
+export { default as authCredentials } from "./auth-credentials";
+export { default as generateQuery } from "./generate-query";
--- services/admin/services/index.ts
+++ services/admin/services/index.ts
@@ -1,5 +1,3 @@
-export { default as authCredentials } from "./auth-credentials";
 export { default as authenticateLogin } from "./authenticate-login";
-export { default as generateQuery } from "./generate-query";
 export { default as isEmailValid } from "./email-validator";
 export { default as renderFeedback } from "./render-feedback";
