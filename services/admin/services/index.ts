a68803ff572b9e33fdf60459b9152316487f3348
Pepeyen <erickfcaldeira@gmail.com>
2021-07-02 00:00:03 -0300

    Introduced text validation service

--- services/admin/services/index.ts
+++ services/admin/services/index.ts
@@ -1,3 +1,4 @@
 export { default as authenticateLogin } from "./authenticate-login";
-export { default as isEmailValid } from "./email-validator";
+export { default as validateEmail } from "./validate-email";
+export { default as validateText } from  "./validate-text";
 export { default as renderFeedback } from "./render-feedback";
