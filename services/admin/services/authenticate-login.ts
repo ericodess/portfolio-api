d7617865ced3b3693f1f1cbbbfb40ff0eea95e03
Pepeyen <erickfcaldeira@gmail.com>
2021-07-02 00:00:30 -0300

    Improved authentication service

--- services/admin/services/authenticate-login.ts
+++ services/admin/services/authenticate-login.ts
@@ -1,15 +1,26 @@
-const authenticateLogin = async (): Promise<boolean> => {
-    const result = await fetch("http://localhost:9005/admin/service/login/auth", {
-              credentials: 'include'
-          })
-          .then(data => data.json())
-          .catch(error => console.log(error));
+const authenticateLogin = async (redirectPath?: string, isAuthenticatedOnly: boolean = false) => {
+	const pageRootPath: string = "http://localhost:9005/admin",
+		  authEndpoint: string = "service/login/auth",
+		  fetchURL: string = `${pageRootPath}/${authEndpoint}`
 
-    if(result && result.isUserAuthenticated){
-        return result.isUserAuthenticated;
-    }else{
-        return false;
-    };
+    await fetch(fetchURL, {
+        credentials: 'include'
+    })
+	.then(response => response.json())
+	.then(result => {
+		if(redirectPath){
+			if(isAuthenticatedOnly){
+				if(result.isUserAuthenticated === false){
+					window.location.replace(`${pageRootPath}/${redirectPath}`);
+				};
+			}else{
+				if(result.isUserAuthenticated){
+					window.location.replace(`${pageRootPath}/${redirectPath}`);
+				};
+			};
+		}
+	})
+    .catch(error => console.log(error));
 };
 
 export default authenticateLogin;
