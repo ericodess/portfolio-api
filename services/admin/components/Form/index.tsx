d38f4f29d2268edf9bad4f649ce6a0480b5bd142
Pepeyen <erickfcaldeira@gmail.com>
2021-07-03 19:45:20 -0300

    Introduced Form component

---
+++ services/admin/components/Form/index.tsx
@@ -0,0 +1,33 @@
+//Types
+import { IForm } from "../../interfaces/component";
+
+//Styles
+import {
+	FormWrapper,
+	FormTitle
+} from "./styles";
+
+const Form = ({
+	title,
+	className,
+	id,
+	method,
+	action,
+	onSubmit,
+	children
+}: IForm ) => {
+	return(
+		<FormWrapper
+			className={className}
+			id={id}
+			method={method}
+			action={action}
+			onSubmit={onSubmit}
+		>
+			<FormTitle>{title}</FormTitle>
+			{children}
+		</FormWrapper>
+	);
+};
+
+export default Form;
