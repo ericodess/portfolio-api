1c75b70f5dd57d7e5c839e06f577e673c63b0033
Pepeyen <erickfcaldeira@gmail.com>
2021-07-03 19:45:08 -0300

    Introduced Feedback component

---
+++ services/admin/components/Feedback/index.tsx
@@ -0,0 +1,13 @@
+//Types
+import { IFeedback } from "../../interfaces/component";
+
+//Styles
+import { FeedbackWrapper } from "./styles";
+
+const Feedback = ({ text, disabled }: IFeedback) => {
+	return(
+		<FeedbackWrapper className={disabled ? "--hidden" : "--shaking"}>{text}</FeedbackWrapper>
+	);
+};
+
+export default Feedback;
