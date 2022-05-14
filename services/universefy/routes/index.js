0f7494d64e78a56fb401f61426001da9d6432884
ericodess <erickfcaldeira@gmail.com>
2022-05-14 05:47:15 -0300

    Updated project file structure

--- services/universefy/routes/index.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-
-const router = express.Router();
-
-const universefy = require('./universefy');
-
-router.use('/universefy', universefy);
-
-module.exports = router;
