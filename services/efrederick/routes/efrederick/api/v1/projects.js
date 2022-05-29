67117861dff28230b13605433bfa969370574a8e
Erick Frederick <erickfcaldeira@gmail.com>
2022-05-29 02:16:26 -0300

    Update projects.js

--- services/efrederick/routes/efrederick/api/v1/projects.js
+++ services/efrederick/routes/efrederick/api/v1/projects.js
@@ -39,7 +39,7 @@ router.get('/', (req, res) => {
 
 				    myHeaders.append('authorization', `token ${result[0].api_key_value}`);
 
-                    getRepoList('ericodess', myHeaders)
+                    getRepoList('ericodesu', myHeaders)
                     .then(projects => {
                         projects.forEach(repository => {
                             if(typeof repository.homepage !== 'undefined' && repository.homepage !== null && repository.homepage !== '' && repository.name !== 'efrederick'){
