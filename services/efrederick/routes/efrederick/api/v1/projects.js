7dc79da2e78d1bcd5860c4ee00f6ce1e777a73b0
ericodess <erickfcaldeira@gmail.com>
2022-11-26 10:29:37 -0300

    Merge remote-tracking branch 'origin/master' into development

---
+++ services/efrederick/routes/efrederick/api/v1/projects.js
@@ -0,0 +1,124 @@
+const express = require('express');
+
+//Models
+const {
+    getConnection,
+    getQuery
+} = require('../../../../models');
+
+const router = express.Router();
+
+//Services
+const getRepoList = require('../../../../services').getRepoList;
+const getRepoContributors = require('../../../../services').getRepoContributors;
+
+router.get('/', (req, res) => {
+    getConnection(async (error,connection) => {
+        if(!error && connection){
+            await getQuery(connection, {
+                queryRequest:{
+                    id: process.env.GITHUB_API_SCHEMA_NAME,
+                    origin: process.env.GITHUB_API_ORIGIN
+                },
+                queryTargetItems: 'api_key_value',
+                queryTargetItemsPrefix: 'api_key',
+                queryTargetTable: 'api_keys',
+                queryIsBinary: true,
+                queryIsLimitless: true
+            })
+            .then(result => {
+                if(result.length === 0){
+                    res.status(401).json({
+                        success: false,
+                        description: 'Invalid key data'
+                    });
+                }else{
+                    const {Headers} = require('node-fetch');
+                    const myHeaders = new Headers();
+                    const projectsSorted = [];
+
+				    myHeaders.append('authorization', `token ${result[0].api_key_value}`);
+
+                    getRepoList('ericodesu', myHeaders)
+                    .then(projects => {
+                        projects.forEach(repository => {
+                            if(typeof repository.homepage !== 'undefined' && repository.homepage !== null && repository.homepage !== '' && repository.name !== 'efrederick'){
+                                projectsSorted.push({
+                                    repositoryName: repository.name,
+                                    reporitoryThumbnailURL: `https://raw.githubusercontent.com/${repository.owner.login}/${repository.name}/master/.github/images/project-thumbnail.png`,
+                                    repositoryURL: repository.html_url,
+                                    repositoryDemoURL: repository.homepage
+                                });
+                            }
+                        });
+
+                        return projects;
+                    })
+                    .then(data => {
+                        return data.map(currentRepo => getRepoContributors('ericodesu', currentRepo.name, myHeaders)
+                        .then(contributors => contributors
+                            .map(contributor => contributor.weeks
+                                .reduce((lineCount, week) => {
+                                    if(week.a <= 10000){
+                                        return lineCount + week.a - week.d;
+                                    }else{
+                                        return lineCount + 0;
+                                    }
+                                    
+                                }, 0)
+                            )
+                        )
+                        .then(lineCounts => {
+                            return lineCounts.reduce((lineTotal, lineCount) => {
+                                return lineTotal + lineCount;
+                            });
+                        })
+                        .then(lines => {
+                            return lines;
+                        }))
+                    })
+                    .then(result => {
+                        return Promise.all(result);
+                    })
+                    .then(linesOfCodeList => {
+                        let totalLinesOfCode = 0, totalNumberOfRepos = 0;
+                        
+                        linesOfCodeList.map(currentRepoLinesOfCode => totalLinesOfCode = totalLinesOfCode + currentRepoLinesOfCode);
+
+                        totalNumberOfRepos = linesOfCodeList.length;
+
+                        res.status(200).json({
+                            success: true,
+                            projects: {
+                                count: totalNumberOfRepos,
+                                totalLOC: totalLinesOfCode,
+                                list: projectsSorted
+                            }
+                        });
+                    })
+                    .catch((error) => {
+                        res.status(500).json({
+                            success: false,
+                            description: 'Server error, please try again'
+                        });
+                    })
+                }
+            })
+            .catch(() => {
+                res.status(500).json({
+                    success: false,
+                    description: 'Server error, please try again'
+                });
+            })
+            
+            connection.release();
+        }else{
+            res.status(500).json({
+                success: false,
+                description: 'Server error, please try again'
+            });
+        }
+    });
+});
+
+module.exports = router;
