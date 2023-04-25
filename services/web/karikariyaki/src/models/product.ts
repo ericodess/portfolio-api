95940590ed85e988dd2c731a1b5814d59c8c59e0
ericodesu <erickfcaldeira@gmail.com>
2023-04-25 19:47:57 -0300

    Implemented the product route

--- services/web/karikariyaki/src/models/product.ts
+++ services/web/karikariyaki/src/models/product.ts
@@ -3,22 +3,60 @@ import { Schema, model } from "mongoose";
 // Types
 import { Statics } from "@types";
 
+// Models
+import { VariantModel } from "@models";
+
+// Services
+import { DatabaseService, StringService } from "@services";
+
 const validateProductName = async (name: string) => {
     if (
-        name.trim().length < Statics.PRODUCT_NAME_MIN_LENGTH ||
-        name.trim().length > Statics.PRODUCT_NAME_MAX_LENGTH
+        StringService.isStringInsideBoundaries(
+            name,
+            Statics.PRODUCT_NAME_MIN_LENGTH,
+            Statics.PRODUCT_NAME_MAX_LENGTH
+        ) === false
     ) {
         if (name.trim().length < Statics.PRODUCT_NAME_MIN_LENGTH) {
-            throw Error("Product name is shorter than 5 characters");
+            throw Error(
+                `Product name is shorter ${Statics.PRODUCT_NAME_MIN_LENGTH} than characters`
+            );
         }
 
-        throw Error("Product name is longer than 25 characters");
+        throw Error(
+            `Product name is longer than ${Statics.PRODUCT_NAME_MAX_LENGTH} characters`
+        );
     }
 
-    const entry = await ProductModel.findOne({ name });
+    const entry = await ProductModel.findOne({
+        name: DatabaseService.generateCaseInsensivitySettings(name),
+    });
 
     if (entry) {
         throw Error("Product name is duplicated");
+    }
+};
+
+const validateProductVariants = async (variantIds: Schema.Types.ObjectId[]) => {
+    for (const variantId of variantIds) {
+        const convertedVariantId = variantId.toString().trim();
+        const foundVariant = await VariantModel.findById(convertedVariantId);
+
+        if (!foundVariant) {
+            throw Error("Product variant ID is invalid");
+        }
+
+        const foundVariantIds = [
+            variantIds.find(
+                (_) =>
+                    _.toString().trim().toLowerCase().replace(" ", "") ===
+                    convertedVariantId.toLowerCase().replace(" ", "")
+            ),
+        ];
+
+        if (foundVariantIds.length >= 1) {
+            throw Error("Product variant is duplicated");
+        }
     }
 };
 
@@ -28,7 +66,11 @@ const ProductSchema = new Schema({
         required: [true, "Product name is required"],
         validate: validateProductName,
     },
-    variantIds: [String],
+    variants: {
+        type: [{ type: Schema.Types.ObjectId, ref: VariantModel }],
+        default: [],
+        validate: validateProductVariants,
+    },
 });
 
 const ProductModel = model("products", ProductSchema);
---
+++ services/web/karikariyaki/src/routes/karikariyaki/api/v1/admin/registry/product/product.ts
@@ -0,0 +1,110 @@
+import { Router } from "express";
+
+// Services
+import {
+    RequestService,
+    ResponseService,
+    ProductService,
+    StringService,
+} from "@services";
+
+const router = Router();
+
+router.get("/", (req, res) => {
+    ProductService.query({
+        id: RequestService.isValidQueryParam(req.query.id)
+            ? (req.query.id as string)?.trim()
+            : null,
+        name: RequestService.isValidQueryParam(req.query.name)
+            ? (req.query.name as string)?.trim()
+            : null,
+    })
+        .then((response) => {
+            res.status(200).json(
+                ResponseService.generateSucessfulResponse(response)
+            );
+        })
+        .catch((error) => {
+            res.status(500).json(
+                ResponseService.generateFailedResponse(error.message)
+            );
+        });
+});
+
+router.post("/", (req, res) => {
+    const name = req.body.name;
+    const variantIds = req.body.variantIds;
+
+    if (!name) {
+        res.status(400).json(
+            ResponseService.generateFailedResponse("Invalid product data")
+        );
+
+        return;
+    }
+
+    ProductService.save({
+        name: name,
+        variants: variantIds
+            ? StringService.toObjectIds(variantIds as string[]) ?? []
+            : [],
+    })
+        .then(() => {
+            res.status(200).json(ResponseService.generateSucessfulResponse());
+        })
+        .catch((error) => {
+            res.status(500).json(
+                ResponseService.generateFailedResponse(error.message)
+            );
+        });
+});
+
+router.patch("/:id", (req, res) => {
+    const id = req.params.id;
+    const name = req.body.name;
+    const variantIds = req.body.variantIds;
+
+    if (!id) {
+        res.status(400).json(
+            ResponseService.generateFailedResponse("Invalid product data")
+        );
+
+        return;
+    }
+
+    ProductService.update(id, { name: name, variants: variantIds })
+        .then((response) => {
+            res.status(200).json(
+                ResponseService.generateSucessfulResponse(response)
+            );
+        })
+        .catch((error) => {
+            res.status(500).json(
+                ResponseService.generateFailedResponse(error.message)
+            );
+        });
+});
+
+router.delete("/:id", (req, res) => {
+    const id = req.params.id;
+
+    if (!id) {
+        res.status(400).json(
+            ResponseService.generateFailedResponse("Invalid product data")
+        );
+
+        return;
+    }
+
+    ProductService.delete(id)
+        .then(() => {
+            res.status(200).json(ResponseService.generateSucessfulResponse());
+        })
+        .catch((error) => {
+            res.status(500).json(
+                ResponseService.generateFailedResponse(error.message)
+            );
+        });
+});
+
+export default router;
