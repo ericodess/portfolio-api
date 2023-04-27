c099408a9cb9a754750daa2355662d859bd24a28
ericodesu <erickfcaldeira@gmail.com>
2023-04-27 16:25:47 -0300

    Implemente failsafe to the connection getter

--- services/web/karikariyaki/src/services/database.ts
+++ services/web/karikariyaki/src/services/database.ts
@@ -11,13 +11,20 @@ const dbURI = `mongodb://${
         : "127.0.0.1:27017/karikariyaki"
 }`;
 
-const connection = mongoose.connect(dbURI, {
+let connection = mongoose.connect(dbURI, {
     minPoolSize: Number(process.env.DATABASE_MIN_POOL_SIZE) ?? 5,
     maxPoolSize: Number(process.env.DATABASE_MAX_POOL_SIZE) ?? 15,
 });
 
 export class DatabaseService {
     public static getConnection() {
+        if (mongoose.connection.readyState === 0) {
+            connection = mongoose.connect(dbURI, {
+                minPoolSize: Number(process.env.DATABASE_MIN_POOL_SIZE) ?? 5,
+                maxPoolSize: Number(process.env.DATABASE_MAX_POOL_SIZE) ?? 15,
+            });
+        }
+
         return connection;
     }
 
