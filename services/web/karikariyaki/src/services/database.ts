95377b09464c1e2d74f00ecd9089abc5fd486ebd
ericodesu <erickfcaldeira@gmail.com>
2023-04-26 21:09:06 -0300

    Improved dabatase connection logic

--- services/web/karikariyaki/src/services/database.ts
+++ services/web/karikariyaki/src/services/database.ts
@@ -1,25 +1,43 @@
 import mongoose from "mongoose";
 
-export class DatabaseService {
-    private static _dbURI = `mongodb://${
-        process.env.DATABASE_HOST.trim().length > 0 &&
-        process.env.DATABASE_PORT.trim().length > 0 &&
-        process.env.DATABASE_TABLE.trim().length > 0
-            ? `${process.env.DATABASE_HOST.trim()}:${process.env.DATABASE_PORT.trim()}/${process.env.DATABASE_TABLE.trim()}`
-            : "127.0.0.1:27017/portfolio_okonomiyator"
-    }`;
+// Services
+import { DateService } from "@services";
 
+const dbURI = `mongodb://${
+    process.env.DATABASE_HOST.trim().length > 0 &&
+    process.env.DATABASE_PORT.trim().length > 0 &&
+    process.env.DATABASE_TABLE.trim().length > 0
+        ? `${process.env.DATABASE_HOST.trim()}:${process.env.DATABASE_PORT.trim()}/${process.env.DATABASE_TABLE.trim()}`
+        : "127.0.0.1:27017/karikariyaki"
+}`;
+
+const connection = mongoose.connect(dbURI, {
+    minPoolSize: Number(process.env.DATABASE_MIN_POOL_SIZE) ?? 5,
+    maxPoolSize: Number(process.env.DATABASE_MAX_POOL_SIZE) ?? 15,
+});
+
+export class DatabaseService {
     public static getConnection() {
-        return mongoose.connect(DatabaseService._dbURI, {
-            minPoolSize: Number(process.env.DATABASE_MIN_POOL_SIZE) ?? 5,
-            maxPoolSize: Number(process.env.DATABASE_MAX_POOL_SIZE) ?? 15,
-        });
+        return connection;
     }
 
-    public static generateCaseInsensivitySettings(value: string) {
+    public static generateBroadQuery(value: string) {
         return {
-            $regex: `^${value.trim().toLowerCase()}$`,
+            $regex: value.trim(),
             $options: "i",
+        };
+    }
+
+    public static generateExactInsensitiveQuery(value: string) {
+        return {
+            $regex: `^${value.trim()}$`,
+            $options: "i",
+        };
+    }
+
+    public static generateStandarizedDateQuery(value: Date) {
+        return {
+            $eq: DateService.standarizeCurrentDate(value).toString(),
         };
     }
 }
