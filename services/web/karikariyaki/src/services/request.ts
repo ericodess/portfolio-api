ecdb578a7b0dc53f3a2cb4a8c6eb82839403a61b
ericodesu <erickfcaldeira@gmail.com>
2023-04-26 21:06:55 -0300

    Added more options to convert from query params

--- services/web/karikariyaki/src/services/request.ts
+++ services/web/karikariyaki/src/services/request.ts
@@ -1,5 +1,28 @@
+// Services
+import { StringService } from "./string";
+
 export class RequestService {
     public static isValidQueryParam(value: any) {
-        return value != null && typeof value == "string";
+        return (
+            value != null && typeof value == "string" && value.trim().length > 0
+        );
+    }
+
+    public static queryParamToBoolean(value: any) {
+        return RequestService.isValidQueryParam(value)
+            ? StringService.toBoolean(value as string)
+            : null;
+    }
+
+    public static queryParamToString(value: any) {
+        return RequestService.isValidQueryParam(value)
+            ? (value as string).trim()
+            : null;
+    }
+
+    public static queryParamToDate(value: any) {
+        return RequestService.isValidQueryParam(value)
+            ? new Date((value as string).trim())
+            : null;
     }
 }
