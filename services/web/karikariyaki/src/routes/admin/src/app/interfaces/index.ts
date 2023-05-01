1c592f90e0def069457ce54f5cfaa3e562582f9c
ericodesu <erickfcaldeira@gmail.com>
2023-05-01 02:22:04 -0300

    Introduced response wrapper interface

---
+++ services/web/karikariyaki/src/routes/admin/src/app/interfaces/index.ts
@@ -0,0 +1,7 @@
+// API
+export { ApiResponseWrapper } from './api/response-wrapper';
+export { Event } from './api/event/event';
+export { EventOrder } from './api/event/order';
+export { Operator } from './api/operator';
+export { Product } from './api/product/product';
+export { ProductVariant } from './api/product/variant';
