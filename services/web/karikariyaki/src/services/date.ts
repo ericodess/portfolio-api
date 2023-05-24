8bb7933d8bf995c0933b1a0069458421e4551fe4
pepeien <erickfcaldeira@gmail.com>
2023-05-24 00:24:30 -0300

    Improved data standardization method

--- services/web/karikariyaki/src/services/date.ts
+++ services/web/karikariyaki/src/services/date.ts
@@ -1,9 +1,9 @@
 export class DateService {
     public static standarizeCurrentDate(targetDate = new Date()): Date {
-        return new Date(
-            targetDate.getFullYear(),
-            targetDate.getMonth(),
-            targetDate.getDate()
-        );
+        const standarizedDate = new Date(targetDate.getTime());
+
+        standarizedDate.setHours(0, 0, 0, 0);
+
+        return standarizedDate;
     }
 }
