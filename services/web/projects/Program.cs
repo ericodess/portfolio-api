2a1979784d7fb9eaaa3102460ff50ff73c7ee6db
pepeien <erickfcaldeira@gmail.com>
2023-05-29 19:18:46 -0300

    Implemented cors to the projects services

--- services/web/projects/Program.cs
+++ services/web/projects/Program.cs
@@ -1,7 +1,19 @@
 using Projects.Services;
 using Projects.Types.Models;
 
+var originsSettingsName = "_originsSettings";
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(
+		name: originsSettingsName,
+        policy  =>
+        {
+            policy.WithOrigins( "http://localhost");
+        });
+});
 
 // Add services to the container.
 
@@ -14,6 +26,8 @@ builder.Services.AddSingleton<ApiKeysService>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+
+app.UseCors(originsSettingsName);
 
 app.UseAuthorization();
 
