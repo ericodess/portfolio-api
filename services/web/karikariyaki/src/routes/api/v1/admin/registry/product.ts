f61d4969b10b99627fe7f835ce8587baaf0c1b79
pepeien <erickfcaldeira@gmail.com>
2023-06-13 22:48:24 -0300

    Improved product creation & edition security

--- services/web/karikariyaki/src/routes/api/v1/admin/registry/product.ts
+++ services/web/karikariyaki/src/routes/api/v1/admin/registry/product.ts
@@ -3,115 +3,111 @@ import { Operator } from "karikarihelper";
 
 // Types
 import { ProductErrors } from "@models";
+import { InHouseError } from "@types";
 
 // Services
 import { RequestService, ResponseService, ProductService } from "@services";
 
 const router = Router();
 
-router.get("/", (req, res) => {
-    ProductService.query(res.locals.operator as Operator, {
-        id: RequestService.queryParamToString(req.query.id),
-        name: RequestService.queryParamToString(req.query.name),
-        realmId: RequestService.queryParamToString(req.query.realmId),
-    })
-        .then((response) => {
-            res.status(200).json(
-                ResponseService.generateSucessfulResponse(response)
-            );
-        })
-        .catch((error) => {
-            res.status(error.code ?? 500).json(
-                ResponseService.generateFailedResponse(error.message)
-            );
-        });
-});
-
-router.post("/", (req, res) => {
-    const name = RequestService.queryParamToString(req.body.name);
-    const realmId = RequestService.queryParamToString(req.body.realmId);
-
-    if (!name || !realmId) {
-        res.status(400).json(
-            ResponseService.generateFailedResponse(ProductErrors.INVALID)
-        );
-
-        return;
-    }
-
-    ProductService.save(res.locals.operator as Operator, {
-        name: name,
-        realmId: realmId,
-    })
-        .then((response) => {
-            res.status(200).json(
-                ResponseService.generateSucessfulResponse(response)
-            );
-        })
-        .catch((error) => {
-            res.status(error.code ?? 500).json(
-                ResponseService.generateFailedResponse(error.message)
-            );
-        });
-});
-
-router.patch("/:id", (req, res) => {
-    const id = req.params.id;
-    const name = RequestService.queryParamToString(req.body.name);
-
-    if (!id) {
-        res.status(400).json(
-            ResponseService.generateFailedResponse(ProductErrors.INVALID)
-        );
-
-        return;
-    }
-
-    ProductService.update(res.locals.operator as Operator, id, { name: name })
-        .then((response) => {
-            res.status(200).json(
-                ResponseService.generateSucessfulResponse(response)
-            );
-        })
-        .catch((error) => {
-            res.status(error.code ?? 500).json(
-                ResponseService.generateFailedResponse(error.message)
-            );
-        });
-});
-
-router.delete("/:id", (req, res) => {
-    const id = req.params.id;
-
-    if (!id) {
-        res.status(400).json(
-            ResponseService.generateFailedResponse(ProductErrors.INVALID)
-        );
-
-        return;
-    }
-
-    ProductService.delete(res.locals.operator as Operator, id)
-        .then((response) => {
-            if (!response) {
-                res.status(404).json(
-                    ResponseService.generateFailedResponse(
-                        ProductErrors.NOT_FOUND
-                    )
-                );
-
-                return;
+router.get("/", async (req, res) => {
+    try {
+        const foundProducts = await ProductService.query(
+            res.locals.operator as Operator,
+            {
+                id: RequestService.queryParamToString(req.query.id),
+                name: RequestService.queryParamToString(req.query.name),
+                realmId: RequestService.queryParamToString(req.query.realmId),
             }
+        );
 
-            res.status(200).json(
-                ResponseService.generateSucessfulResponse(response)
-            );
-        })
-        .catch((error) => {
-            res.status(error.code ?? 500).json(
-                ResponseService.generateFailedResponse(error.message)
-            );
-        });
+        res.status(200).json(
+            ResponseService.generateSucessfulResponse(foundProducts)
+        );
+    } catch (error) {
+        res.status(error.code ?? 500).json(
+            ResponseService.generateFailedResponse(error.message)
+        );
+    }
+});
+
+router.post("/", async (req, res) => {
+    try {
+        const name = RequestService.queryParamToString(req.body.name);
+        const realmId = RequestService.queryParamToString(req.body.realmId);
+
+        if (!name) {
+            throw new InHouseError(ProductErrors.INVALID, 400);
+        }
+
+        const response = await ProductService.save(
+            res.locals.operator as Operator,
+            {
+                name: name,
+                realmId: realmId,
+            }
+        );
+
+        res.status(200).json(
+            ResponseService.generateSucessfulResponse(response)
+        );
+    } catch (error) {
+        res.status(error.code ?? 500).json(
+            ResponseService.generateFailedResponse(error.message)
+        );
+    }
+});
+
+router.patch("/:id", async (req, res) => {
+    try {
+        const id = req.params.id;
+        const name = RequestService.queryParamToString(req.body.name);
+
+        if (!id) {
+            throw new InHouseError(ProductErrors.INVALID, 400);
+        }
+
+        const response = await ProductService.update(
+            res.locals.operator as Operator,
+            id,
+            { name: name }
+        );
+
+        res.status(200).json(
+            ResponseService.generateSucessfulResponse(response)
+        );
+    } catch (error) {
+        res.status(error.code ?? 500).json(
+            ResponseService.generateFailedResponse(error.message)
+        );
+    }
+});
+
+router.delete("/:id", async (req, res) => {
+    try {
+        const id = req.params.id;
+
+        if (!id) {
+            throw new InHouseError(ProductErrors.INVALID, 400);
+        }
+
+        const response = await ProductService.delete(
+            res.locals.operator as Operator,
+            id
+        );
+
+        if (!response) {
+            throw new InHouseError(ProductErrors.NOT_FOUND, 404);
+        }
+
+        res.status(200).json(
+            ResponseService.generateSucessfulResponse(response)
+        );
+    } catch (error) {
+        res.status(error.code ?? 500).json(
+            ResponseService.generateFailedResponse(error.message)
+        );
+    }
 });
 
 export default router;
