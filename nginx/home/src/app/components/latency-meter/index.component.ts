9464ee6e77f94904ec4a7a429e2aebd04e0faf14
ericodesu <erickfcaldeira@gmail.com>
2023-07-09 23:02:16 -0300

    Implemented latency clipping to the latency meter

--- nginx/home/src/app/components/latency-meter/index.component.ts
+++ nginx/home/src/app/components/latency-meter/index.component.ts
@@ -5,9 +5,10 @@ import { Component, Input } from '@angular/core';
 	templateUrl: './index.component.html',
 })
 export class LatencyMeterComponent {
-	private readonly HIGH_MAX_LATENCY_IN_MS = 300;
-	private readonly MID_MIN_LATENCY_IN_MS = 100;
+	private readonly HIGH_MAX_LATENCY_IN_MS = 999;
 	private readonly MID_MAX_LATENCY_IN_MS = 200;
+	private readonly LOW_MAX_LATENCY_IN_MS = 100;
+	private readonly LATENCY_UNIT_OF_MEASUREMENT = 'ms';
 
 	@Input()
 	public latency: number = -1;
@@ -17,17 +18,22 @@ export class LatencyMeterComponent {
 			return '--dead';
 		}
 
-		if (this.latency >= this.HIGH_MAX_LATENCY_IN_MS) {
+		if (this.latency > this.MID_MAX_LATENCY_IN_MS) {
 			return '--high';
 		}
 
-		if (
-			this.latency >= this.MID_MIN_LATENCY_IN_MS &&
-			this.latency <= this.MID_MAX_LATENCY_IN_MS
-		) {
+		if (this.latency > this.LOW_MAX_LATENCY_IN_MS) {
 			return '--mid';
 		}
 
 		return '--normal';
+	}
+
+	public getLatency(): string {
+		if (this.latency > this.HIGH_MAX_LATENCY_IN_MS) {
+			return `${this.HIGH_MAX_LATENCY_IN_MS}+ ${this.LATENCY_UNIT_OF_MEASUREMENT}`;
+		}
+
+		return `${this.latency} ${this.LATENCY_UNIT_OF_MEASUREMENT}`;
 	}
 }
